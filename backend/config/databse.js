460124813b43572100577bdb7a98127c4e4caa8f
abhaytagad <omshiv09.ani@gmail.com>
2024-04-24 19:24:22 +0530

    modifying database.js

--- backend/config/databse.js
+++ backend/config/databse.js
@@ -3,5 +3,7 @@ const mongoose = require('mongoose')
 exports.dbConnect = ()=>{
     mongoose.connect(process.env.DB_URL)
     .then(()=>{console.log("DB conected")})
-    .catch((e)=>{console.log("could not conect Succesfully")})
+    .catch((e)=>{
+        console.log("could not conect Succesfully")
+    console.log(e)})
 }
