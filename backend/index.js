5c07cab6f14132c32e316973a6e855433ac0bc67
abhaytagad <abhaytagad2812@gmail.com>
2025-08-27 21:35:46 +0530

    adding Frontend

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,16 @@ app.use(
   })
 );
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://iteractvepdfchatboat.vercel.app"
+];
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
