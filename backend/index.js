768d889f9efa0849ef99d63ab6f7dfb46f24d1d3
abhaytagad <abhaytagad2812@gmail.com>
2025-08-28 16:02:48 +0530

    updating croc

--- backend/index.js
+++ backend/index.js
@@ -16,14 +16,22 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://iteractvepdfchatboat-t2ki.vercel.app"
+];
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000",'https://iteractvepdfchatboat-t2ki.vercel.app');
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
+
 
 app.listen(process.env.PORT, () => {
   console.log("server Started");
