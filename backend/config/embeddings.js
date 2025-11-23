ed6787ff7206887886a0a69b474c58a4e648eca3
Abhay Tagad <abhaytagad2812@gmail.com>
2025-11-23 23:30:28 +0530

    Update embeddings.js

--- backend/config/embeddings.js
+++ backend/config/embeddings.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 console.log(process.env.GOOGLE_API_KEY)
 // Gemini Embeddings Wrapper
 const embeddings = new GoogleGenerativeAIEmbeddings({
-  model: "models/embedding-001",   // official Gemini embeddings model
+  model: "text-embedding-004",   // official Gemini embeddings model
   apiKey: process.env.GOOGLE_API_KEY,
 });
 
